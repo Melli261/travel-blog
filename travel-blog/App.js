07683dea10d27616020ca2a25046f9d8b361cc0f
Melanie <122368878+Melli261@users.noreply.github.com>
2024-11-19 17:24:46 +0100

    Delete travel-blog directory

--- travel-blog/App.js
+++
@@ -1,9 +0,0 @@
-function scrollLeft(id) {
-    var gallery = document.getElementById(id);
-    gallery.scrollBy({ left: -300, behavior: 'smooth' });
-}
-
-function scrollRight(id) {
-    var gallery = document.getElementById(id);
-    gallery.scrollBy({ left: 300, behavior: 'smooth' });
-}
